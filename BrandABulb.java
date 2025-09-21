8055be7eb671f0b85a2b95379720ccd8686974ef
umvirrrr <Umair.ahmed1@ontariotechu.net>
2025-09-21 17:18:29 -0400

    Delete BrandABulb.java

--- BrandABulb.java
+++
@@ -1,8 +0,0 @@
-package products;
-
-public class BrandABulb extends SmartBulb {
-    @Override
-    public void displayInfo() {
-        System.out.println("Brand A Smart Bulb | Power Usage: " + powerUsage + "W");
-    }
-}
