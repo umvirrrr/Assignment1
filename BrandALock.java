2455bd153c3874d4a8b3e99c1f70bf2410b9610a
umvirrrr <Umair.ahmed1@ontariotechu.net>
2025-09-21 17:18:50 -0400

    Delete BrandALock.java

--- BrandALock.java
+++
@@ -1,8 +0,0 @@
-package products;
-
-public class BrandALock extends SmartLock {
-    @Override
-    public void displayInfo() {
-        System.out.println("Brand A Smart Lock | Battery Consumption: " + batteryConsumption + "%");
-    }
-}
